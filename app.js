8ba7324b7d33fb00d90e75fdfa8721b2530d4233
iamrish <rishabhmalhotra57@gmail.com>
2021-06-27 12:31:05 +0530

    updated html and css

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 const title = document.getElementById("title");
+const underscore = document.createElement("span");
+const about = document.getElementById("about-desc");
 
-title.textContent = "THE | \n\tYOGA | \n\t\tCLINIC"
+underscore.id = "moving-ele";
+underscore.textContent = "_";
+title.textContent = "THE | \n\tYOGA | \n\t\t   CLINIC _/\\";
+
+title.appendChild(underscore);
+let cursor = true;
+let speed = 300;
+setInterval(() => {
+  if (cursor) {
+    underscore.style.opacity = 0;
+    cursor = false;
+  } else {
+    underscore.style.opacity = 1;
+    cursor = true;
+  }
+}, speed);
+
